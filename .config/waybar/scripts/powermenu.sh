729e37a8bfbb0ad58e13b9842cf77e89b30bec50
Bhavesh163 <bhaveshfung@gmail.com>
2025-11-30 14:08:51 +0700

    Updated waybar and hyprland config

--- .config/waybar/scripts/powermenu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-choice=$(echo -e "Shutdown\nRestart\nLogout\nSuspend\nLock" | wofi --show dmenu --prompt="Power Menu")
-case $choice in
-    Shutdown) systemctl poweroff ;;
-    Restart) systemctl reboot ;;
-    Logout) hyprctl dispatch exit ;;
-    Suspend) systemctl suspend ;;
-    Lock) swaylock ;;
-esac
